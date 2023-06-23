93fb184e981eefd5c75562f7ac3f574b21b3a285
Rich Nyman <rich.nyman@cobalt.net>
2023-06-23 16:00:48 -0400

    asd

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./css/index.scss";
 import Home from "./pages/Home.tsx";
 
 ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,33 +1,40 @@
 import logo from "../assets/logo.svg";
+import TopNav from "../components/TopNav";
 
 const Home = () => {
   return (
-    <>
+    <center-l>
       <header>
-        <div class="container">
-          <div class="topNav">
-            <div>
-              <a href="#">
-                <img src={logo} width="50"></img>
-              </a>
-              <nav>
-                <ul>
-                  <li>nav 1</li>
-                  <li>nav 1</li>
-                  <li>nav 1</li>
-                  <li>nav 1</li>
-                  <li>nav 1</li>
-                </ul>
-              </nav>
-            </div>
-          </div>
+        <div className="container">
+          <cluster-l justify="space-between" align="center" space="var(--s1)">
+            <a href="/">
+              <img
+                src="src/assets/logo.svg"
+                alt="logo"
+                width="50"
+                height="50"
+              />
+            </a>
+            <TopNav />
+          </cluster-l>
         </div>
       </header>
-      <main>
-        <div class="container">Text sample</div>
+      <main className="blackout padding-throughout">
+        <div className="container">
+          <stack-l recursive space="2rem">
+            <h1>Hello, I'm Rich.</h1>
+            <p>I make things</p>
+            <p>Lorem ipsum dolor sit amet.</p>
+          </stack-l>
+        </div>
       </main>
-      <footer></footer>
-    </>
+      <footer className="grayout">
+        <div className="container">
+          <h2>footer content</h2>
+          <box-l borderWidth="var(--border-thin)">This is a box</box-l>
+        </div>
+      </footer>
+    </center-l>
   );
 };
 
