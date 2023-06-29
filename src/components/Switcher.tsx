3c0d8839ce72bd6467125aad95fc6432e3b260d4
Rich Nyman <richnyman@gmail.com>
2023-06-28 23:56:02 -0400

    frame

--- src/components/Switcher.tsx
+++ src/components/Switcher.tsx
@@ -1,18 +1,5 @@
 const Switcher = () => {
-  return (
-    <main>
-      <section id="switcher-figurine" className="blackines">
-        <h2>The Switcher</h2>
-      </section>
-      <section id="switcher-demo" className="grayout whitelines">
-        <div className="switcher">
-          <div className="switches">side</div>
-          <div className="switches">feature</div>
-          <div className="switches">side</div>
-        </div>
-      </section>
-    </main>
-  );
+  return <main></main>;
 };
 
 export default Switcher;
